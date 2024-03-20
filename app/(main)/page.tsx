df9dba84cebb95c249cd6a57b11229be3144d331
bysxx <0415white@gmail.com>
2024-03-20 15:18:18 +0900

    feat: init project

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,7 @@
 export default function MainPage() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center p-8">
-      <h1 className="mb-4 text-5xl font-bold">Welcome to Next.js 14.1</h1>
-      <p className="text-lg">Next 13 + TypeScript + TailwindCSS</p>
+      <h1 className="mb-4 text-5xl font-bold">Stock Education</h1>
     </main>
   );
 }
--- app/common/components/ui/global-nav.tsx
+++ app/common/components/ui/global-nav.tsx
@@ -4,19 +4,11 @@ function GlobalNav() {
   return (
     <nav className="sticky top-0 grid h-16 w-full grid-cols-2 items-center justify-center bg-[hsla(0,0%,100%,.8)] px-8 py-4 shadow-bottom backdrop-blur-sm backdrop-saturate-150">
       <div className="flex justify-start text-2xl text-gray-900">
-        <Link href="/">Next 14</Link>
+        <Link href="/">Stock</Link>
       </div>
 
       <div className="flex justify-end gap-x-2 text-gray-500">
         <Link href="/example">Example</Link>
-        <a
-          className="ml-2 flex items-center gap-x-1"
-          href="https://github.com/bysxx/next-ts-template-tailwind"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <span>Source Code</span>
-        </a>
       </div>
     </nav>
   );
